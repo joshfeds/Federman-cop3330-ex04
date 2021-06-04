2aa13010154024f2ef425d2dfd5ef16d49bf3e80
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 16:13:41 -0400

    Exercise 04

---
+++ src/main/java/oop/Exercise04/Exercise04.java
@@ -0,0 +1,11 @@
+package oop.Exercise04;
+import java.util.Scanner;
+public class Exercise04 {
+    Scanner input = new Scanner(System.in);
+    public static void EnterNoun() {
+
+    }
+    public static void main(String[] args){
+
+    }
+}
